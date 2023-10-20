31b72038b5489b79a4e3a2678d96652ff4db7070
Aditya Pandit <adityapanditdev@gmail.com>
2023-10-20 17:12:36 +0530

    Added docker configuration

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,28 @@ user2 = User.create(name: 'User 2', email: 'user2@example.com')
 
 # Create Notes for User 1
 5.times do
-  user1.notes.create(name: Faker::Name.name, content: Faker::Lorem.paragraphs)
+  user1.notes.create(name: 'user note 1', content: 'user note 1 content')
 end
 
 # Create Notes for User 2
 5.times do
-  user2.notes.create(name: Faker::Name.name, content: Faker::Lorem.paragraphs)
+  user2.notes.create(name: 'user note 2', content: 'user note 2 content')
 end
 
 # Create Tasks for User 1
 5.times do
   user1.tasks.create(
-    name: Faker::Name.name,
-    description: Faker::Lorem.paragraphs,
-    due_date: Faker::Date.between(from: 2.days.ago, to: Date.today)
+    name: 'user-task-1',
+    description: 'user-task-description-1',
+    due_date: Date.today
   )
 end
 
 # Create Tasks for User 2
 5.times do
   user2.tasks.create(
-    name: Faker::Name.name,
-    description: Faker::Lorem.paragraphs,
-    due_date: Faker::Date.between(from: 2.days.ago, to: Date.today)
+    name: 'user-task-2',
+    description: 'user-task-description-2',
+    due_date: Date.today
   )
 end
